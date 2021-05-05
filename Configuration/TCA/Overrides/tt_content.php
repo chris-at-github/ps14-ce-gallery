6ca52f4a1d9407abdf9966db88b87d24c4fd6af0
Christian Pschorr <c.pschorr@datamints.com>
2021-05-06 00:04:17 +0200

    Konfiguration für verknüpfte Bilder im Modul Gallery hinterlegt

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -15,7 +15,7 @@
 $GLOBALS['TCA']['tt_content']['types']['ce_gallery'] = [
 	'showitem' => '
 			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;header, bodytext, image,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;header, bodytext, tx_xo_file,
 		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
 			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
 		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
@@ -24,6 +24,35 @@ $GLOBALS['TCA']['tt_content']['types']['ce_gallery'] = [
 			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
 		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
 	',
+	'columnsOverrides' => [
+		'tx_xo_file' => [
+			'label' => 'LLL:EXT:ce_gallery/Resources/Private/Language/locallang_tca.xlf:tt_content.tx_xo_file',
+			'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+				'tx_xo_file',
+				[
+					'appearance' => [
+						'collapseAll' => 1,
+						'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:media.addFileReference'
+					],
+					'maxitems' => 999
+				],
+				$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+			),
+		]
+	]
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_gallery']['columnsOverrides']['tx_xo_file']['config']['overrideChildTca']['types'] = [
+	'0' => [
+		'showitem' => '
+			--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+			--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+		'showitem' => '
+			--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+			--palette--;;filePalette'
+	],
 ];
 
 $GLOBALS['TCA']['tt_content']['types']['ce_gallery']['columnsOverrides']['bodytext']['config'] = [
