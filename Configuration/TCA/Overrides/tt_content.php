5aef8f4c527ad549949966fedaf3bc2a41527c12
Christian Pschorr <pschorr.christian@gmail.com>
2022-10-16 00:52:11 +0200

    Crop-Varianten für das Gallery-Bild hinterlegt

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -61,6 +61,37 @@ $GLOBALS['TCA']['tt_content']['types']['ps14_gallery']['columnsOverrides']['tx_x
 	],
 ];
 
+$GLOBALS['TCA']['tt_content']['types']['ps14_gallery']['columnsOverrides']['tx_xo_file']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = [
+	'fullsize' => [
+		'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_crop_variant.fullsize',
+		'allowedAspectRatios' => [
+			'NaN' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
+				'value' => 0.0
+			],
+		],
+		'selectedRatio' => 'NaN',
+	],
+	'thumbnail' => [
+		'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_crop_variant.thumbnail',
+		'allowedAspectRatios' => [
+			'16_9' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.16_9',
+				'value' => 16 / 9
+			],
+			'4_3' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.4_3',
+				'value' => 4 / 3
+			],
+			'1_1' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.1_1',
+				'value' => 1
+			],
+		],
+		'selectedRatio' => '4_3',
+	],
+];
+
 $GLOBALS['TCA']['tt_content']['types']['ps14_gallery']['columnsOverrides']['bodytext']['config'] = [
 	'enableRichtext' => true,
 	'richtextConfiguration' => 'xoDefault',
